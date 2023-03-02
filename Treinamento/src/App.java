a7e5e41dcb4ecbd91f0b3d9c95080de3cd6de90b
CEHL98 <cehlpb@gmail.com>
2023-03-02 16:40:14 -0300

    Primeiro dia de treinamento

---
+++ Treinamento/src/App.java
@@ -0,0 +1,10 @@
+import opensource.beeCrond.questoes.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        //ExtremamenteFacil.resolver();
+       // AreaCirculo.resolver();
+       //SomaSimples.resolver();
+       //ProdutoSimples.resolver();
+       //Media1.resolver();
+    } 
+}
